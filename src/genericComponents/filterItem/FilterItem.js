f28724de18c2351b859447b49dc812d8a67d1532
roman <romans@genesort.com>
2019-05-26 18:31:48 +0300

    antd has been added

--- src/genericComponents/filterItem/FilterItem.js
+++ src/genericComponents/filterItem/FilterItem.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 // import PropTypes from "prop-types";
-import { Checkbox } from 'antd';
-
+import { Checkbox } from "antd";
 
 class FilterItem extends Component {
   render() {
-    return (
-      <Checkbox>xxx</Checkbox>
-    )
+    return <Checkbox>xxx</Checkbox>;
   }
 }
 
