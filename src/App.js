081d1f1114fa10ba9f84da3db562681a67ae4576
Vadim <vadim@grenesort.com>
2019-05-26 13:17:37 +0300

    eslint test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import logo from './logo.svg';
+import './App.scss';
 // import Portal from "GenericComponents/Portal";
 
 function App() {
