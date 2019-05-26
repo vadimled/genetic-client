60e1a1ebd1bf4b562283d54ab4d891b82bf9141a
roman <romans@genesort.com>
2019-05-26 14:53:37 +0300

    husky integration

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
-    const x = 123
   return (
     <div className="App">
-      <header className='App-header'>
-              <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -20,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-     </div>
+    </div>
   );
 }
 
