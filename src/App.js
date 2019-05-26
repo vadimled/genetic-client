6833ca184c1aeabf7af2ed1c6fa93ae1af99ef21
roman <romans@genesort.com>
2019-05-26 14:23:55 +0300

    error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 function App() {
+    const x = 123
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className='App-header'>
+              <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className='App-link'
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
