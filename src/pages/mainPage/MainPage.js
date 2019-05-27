3b017212b96ebe3f37325ff44fe0a771507de840
Vadim <vadim@grenesort.com>
2019-05-27 18:36:23 +0300

    created by css. 3-party component removed

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -1,59 +1,47 @@
 import React, { Component } from "react";
 import style from "./MainPage.module.scss";
-import { OffCanvas, OffCanvasBody, OffCanvasMenu } from "react-offcanvas";
 import cn from "classnames";
-import PageLayout from "./components/pageLayout";
 import SideBarLayout from "./components/sideBarLayout";
-import { ReactComponent as OpendIcon } from "Assets/openedFiltersPanel.svg";
-import { ReactComponent as ClosedIcon } from "Assets/closedFiltersPanel.svg";
-
-// import PropTypes from 'prop-types';
 
 class MainPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { sidebarToggle: true };
+    this.state = {
+      sidebarToggle: false
+    };
   }
 
   handleClick = () => {
-    this.setState({ sidebarToggle: !this.state.sidebarToggle });
+    this.setState({
+      sidebarToggle: !this.state.sidebarToggle
+    });
   };
 
   render() {
     const { sidebarToggle } = this.state;
     return (
       <div className={style["main-page"]}>
-        <OffCanvas
-          className="OffCanvas-wrapper"
-          width={192}
-          transitionDuration={300}
-          effect={"push"}
-          isMenuOpened={sidebarToggle}
-          position={"left"}
+        <div
+          className={cn(["sidebar-wrapper", { "sidebar-open": sidebarToggle }])}
         >
-          <OffCanvasBody
-            className={cn([
-              "main-content-wrapper",
-              { "sidebar-open": sidebarToggle }
-            ])}
-          >
-            <PageLayout />
-            <div className="icon-mode" onClick={this.handleClick}>
-              {sidebarToggle ? <OpendIcon /> : <ClosedIcon />}
-            </div>
-          </OffCanvasBody>
-          <OffCanvasMenu className="sidebar-wrapper">
-            <SideBarLayout title={"Filters"} />
-          </OffCanvasMenu>
-        </OffCanvas>
+          <SideBarLayout
+            title={"Filters"}
+            handleClick={this.handleClick}
+            mode={sidebarToggle}
+          />
+        </div>
+        <div
+          className={cn([
+            "main-content-wrapper",
+            { "sidebar-open": sidebarToggle }
+          ])}
+        >
+          <h1>Page content</h1>
+        </div>
       </div>
     );
   }
 }
-
-// MainPage.propTypes = {
-//
-// };
 
 export default MainPage;
--- src/pages/mainPage/components/sideBarLayout/SideBarLayout.js
+++ src/pages/mainPage/components/sideBarLayout/SideBarLayout.js
@@ -1,12 +1,22 @@
 import React from "react";
 import style from "./SideBarLayout.module.scss";
 import PropTypes from "prop-types";
+import { ReactComponent as OpenedIcon } from "Assets/openedFiltersPanel.svg";
+import { ReactComponent as ClosedIcon } from "Assets/closedFiltersPanel.svg";
+import cn from "classnames";
 
 const SideBarLayout = props => {
-  const { title, subtitle, children } = props;
+  const { title, subtitle, children, handleClick, mode } = props;
 
   return (
     <div className={style["sidebar-layout"]}>
+      <div
+        className={cn(["icon-mode", { "sidebar-closed": !mode }])}
+        onClick={handleClick}
+      >
+        {mode ? <OpenedIcon /> : <ClosedIcon />}
+      </div>
+
       <div className="content">
         <div>
           {title && <div className="title">{title}</div>}
