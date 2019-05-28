fdae4b5d88ccf9e487fe04f8577ba0dc88a8c522
roman <romans@genesort.com>
2019-05-28 13:36:16 +0300

    Collapse has been added

--- src/pages/mainPage/MainPage.js
+++ src/pages/mainPage/MainPage.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 import SelectionGroup from "../../genericComponents/selectionGroup";
 import { TAG_COLORS } from "../../utils/constants";
 import { Icon } from "antd";
-import { Collapse } from 'antd';
-import style from './MainPage.module.scss';
+import { Collapse } from "antd";
+import style from "./MainPage.module.scss";
 
 // eslint-disable-next-line
 const Panel = Collapse.Panel;
@@ -13,86 +13,98 @@ function callback(key) {
   console.log(key);
 }
 
-const Arrow = ({dir}) => <i className={`${dir} arrow`} />;
-
+const Arrow = ({ dir }) => <i className={`${dir} arrow`} />;
 
 class MainPage extends Component {
-
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       variantClassFilterItems: [
-        {tagColor: TAG_COLORS.white, label: 'Unclassified'},
-        {tagColor: TAG_COLORS.red, label: 'PATH'},
-        {tagColor: TAG_COLORS.orange, label: 'LATH'},
-        {tagColor: TAG_COLORS.yellow, label: 'VUS'},
-        {tagColor: TAG_COLORS.blueLight, label: 'LBEN'},
-        {tagColor: TAG_COLORS.blue, label: 'BEN'},
+        { tagColor: TAG_COLORS.white, label: "Unclassified" },
+        { tagColor: TAG_COLORS.red, label: "PATH" },
+        { tagColor: TAG_COLORS.orange, label: "LATH" },
+        { tagColor: TAG_COLORS.yellow, label: "VUS" },
+        { tagColor: TAG_COLORS.blueLight, label: "LBEN" },
+        { tagColor: TAG_COLORS.blue, label: "BEN" }
       ],
 
       typeFilterItems: [
-        {label: 'Somatic'},
-        {label: 'Germline'},
-        {label: 'ACMG'}
+        { label: "Somatic" },
+        { label: "Germline" },
+        { label: "ACMG" }
       ],
 
       hotSpotFilterItems: [
-        {label: 'True', icon: <Icon type="check" />},
-        {label: 'False', icon: <Icon type="close" />}
+        { label: "True", icon: <Icon type="check" /> },
+        { label: "False", icon: <Icon type="close" /> }
       ],
 
       snpFilterItems: [
-        {label: 'True', icon: <Icon type="check" />},
-        {label: 'False', icon: <Icon type="close" />}
+        { label: "True", icon: <Icon type="check" /> },
+        { label: "False", icon: <Icon type="close" /> }
       ],
 
       roiFilterItems: [
-        {label: 'True', icon: <Icon type="check" />},
-        {label: 'False', icon: <Icon type="close" />}
+        { label: "True", icon: <Icon type="check" /> },
+        { label: "False", icon: <Icon type="close" /> }
       ],
 
       gnomIdFilterItems: [
-        {value: 'na', label: "NA"},
-        {value: 'veryRare', label: "very rare(0-1%)"},
-        {value: 'rare', label: "rare (1-5%)"},
-        {value: 'common', label: "common (>5%)"},
+        { value: "na", label: "NA" },
+        { value: "veryRare", label: "very rare(0-1%)" },
+        { value: "rare", label: "rare (1-5%)" },
+        { value: "common", label: "common (>5%)" }
       ]
     };
   }
 
-
-
   render() {
-
     return (
-      <div className={style['main-page-wrapper']}>
+      <div className={style["main-page-wrapper"]}>
         <h3>MainPage</h3>
-
-        <Collapse defaultActiveKey={['1']} onChange={callback} expandIcon={({ isActive }) => <Arrow dir={!isActive ? 'right' : 'down'} />}>
-
+        <Collapse
+          defaultActiveKey={["1"]}
+          onChange={callback}
+          expandIcon={({ isActive }) => (
+            <Arrow dir={!isActive ? "right" : "down"} />
+          )}
+        >
           <Panel header="Type" key="1">
-            <SelectionGroup mode="multiple" filterItems={this.state.variantClassFilterItems}/>
+            <SelectionGroup
+              mode="multiple"
+              filterItems={this.state.variantClassFilterItems}
+            />
           </Panel>
 
           <Panel header="Variant Class" key="2">
-            <SelectionGroup mode="single" filterItems={this.state.typeFilterItems}/>
+            <SelectionGroup
+              mode="single"
+              filterItems={this.state.typeFilterItems}
+            />
           </Panel>
 
           <Panel header="Hot Spot" key="3">
-            <SelectionGroup mode="single" filterItems={this.state.hotSpotFilterItems}/>
+            <SelectionGroup
+              mode="single"
+              filterItems={this.state.hotSpotFilterItems}
+            />
           </Panel>
 
           <Panel header="SNP" key="4">
-            <SelectionGroup mode="single" filterItems={this.state.snpFilterItems}/>
+            <SelectionGroup
+              mode="single"
+              filterItems={this.state.snpFilterItems}
+            />
           </Panel>
 
           <Panel header="ROI" key="5">
-            <SelectionGroup mode="single" filterItems={this.state.roiFilterItems}/>
+            <SelectionGroup
+              mode="single"
+              filterItems={this.state.roiFilterItems}
+            />
           </Panel>
-
-        </Collapse>,
-
+        </Collapse>
+        ,
       </div>
     );
   }
