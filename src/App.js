4f98a393de73300feff4ca03441f48a90f46baa8
Shahar Porat <shahar@genesort.com>
2019-05-28 10:15:27 +0000

    Revert "Merge branch 's1/feature-sidebar-filters/AN-84-Sidebar-Filters-Layout' into 'dev'"
    This reverts merge request !8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import "Css/App.scss";
-import { Route, Switch } from "react-router-dom";
-import MainPage from "Pages/mainPage";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-      </Switch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
