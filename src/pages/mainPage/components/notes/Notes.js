55099b411049b1db83da5a5f35c751abc88228ec
Vadim <vadim@grenesort.com>
2019-06-02 12:58:43 +0300

    actionsFooter component created

--- src/pages/mainPage/components/notes/Notes.js
+++ src/pages/mainPage/components/notes/Notes.js
@@ -1,9 +1,41 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import style from "./Notes.modules.scss";
+import { TEXTS } from "src/utils/constants";
+import { ReactComponent as EditIcon } from "Assets/edit.svg";
 // import PropTypes from 'prop-types';
 
 class Notes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEdit: false
+    };
+  }
+
+  handelEditClick = () => {
+    this.setState({ isEdit: true });
+  };
+
   render() {
-    return <div>Notes</div>;
+    const { valueNotes } = this.props;
+    return (
+      <div className={style["notes-wrapper"]}>
+        {!valueNotes ? (
+          <div className="notes-content-empty" onClick={this.handelEditClick}>
+            {TEXTS.addNote}
+            {this.state.isEdit && <div className="edit-text-box" />}
+          </div>
+        ) : (
+          <Fragment>
+            <div className="notes-content">{valueNotes}</div>
+            <div className="icon-edit" onClick={this.handelEditClick}>
+              <EditIcon />
+            </div>
+          </Fragment>
+        )}
+      </div>
+    );
   }
 }
 
