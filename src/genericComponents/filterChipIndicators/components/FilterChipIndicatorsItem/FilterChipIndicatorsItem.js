cf8cd605b8bcfd1d8dcb8bec8db52840bf9effdf
Roman S <romulus.hund@gmail.com>
2019-06-12 15:11:32 +0300

    cleanup

--- src/genericComponents/filterChipIndicators/components/FilterChipIndicatorsItem/FilterChipIndicatorsItem.js
+++ src/genericComponents/filterChipIndicators/components/FilterChipIndicatorsItem/FilterChipIndicatorsItem.js
@@ -1,43 +1,44 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import style from './FilterChipIndicatorsItem.module.scss';
-import btnImg from 'Assets/close.svg';
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import style from "./FilterChipIndicatorsItem.module.scss";
+import btnImg from "Assets/close.svg";
 import { FILTERS } from "Utils/constants";
 
 const FilterChipIndicatorsItem = ({ data, onDelete, filtersConfigKey }) => {
-
   return (
     <div className={style["filter-chip-indicators-item"]}>
       <div className="value">
         {data.map((item, index) => {
-          if (typeof item === 'number') {
-            return <Fragment key={index}>
-              <span>{item}</span>
-              {index < data.length-1 &&
-                (filtersConfigKey === FILTERS.vaf
-                  ? <span> - </span>
-                  : <span>, </span>
-                )
-              }
-            </Fragment>;
+          if (typeof item === "number") {
+            return (
+              <Fragment key={index}>
+                <span>{item}</span>
+                {index < data.length - 1 &&
+                  (filtersConfigKey === FILTERS.vaf ? (
+                    <span> - </span>
+                  ) : (
+                    <span>, </span>
+                  ))}
+              </Fragment>
+            );
+          } else if (typeof item === "object") {
+            return (
+              <Fragment key={item.id}>
+                {!!item.icon && <span className="icon">{item.icon}</span>}
+                <span>{item.label}</span>
+                {index < data.length - 1 && <span>, </span>}
+              </Fragment>
+            );
+          } else if (typeof item === "string") {
+            return <span key={index}>{item}</span>;
           }
-          else if (typeof item === 'object') {
-            return <Fragment key={item.id}>
-              {!!item.icon && <span className="icon">{item.icon}</span>}
-              <span>{item.label}</span>
-              {index < data.length-1 && <span>, </span>}
-            </Fragment>;
-          }
-          else if (typeof item === 'string') {
-            return <span key={index}>{item}</span>
-          }
-          return '';
+          return "";
         })}
       </div>
 
       <button
         className="button"
-        style={{backgroundImage: `url(${btnImg})`}}
+        style={{ backgroundImage: `url(${btnImg})` }}
         onClick={onDelete}
       />
     </div>
@@ -45,13 +46,15 @@ const FilterChipIndicatorsItem = ({ data, onDelete, filtersConfigKey }) => {
 };
 
 FilterChipIndicatorsItem.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.number, PropTypes.string])).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.object, PropTypes.number, PropTypes.string])
+  ).isRequired,
   onDelete: PropTypes.func.isRequired,
   filtersConfigKey: PropTypes.string
 };
 
 FilterChipIndicatorsItem.defaultProps = {
-  filtersConfigKey: ''
+  filtersConfigKey: ""
 };
 
 export default React.memo(FilterChipIndicatorsItem);
