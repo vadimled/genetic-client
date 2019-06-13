aeba1a2a882dd3066bbb0df5d1beb87bfd036f87
Roman S <romulus.hund@gmail.com>
2019-06-13 17:15:44 +0300

    WIP

--- src/genericComponents/variantTable/VariantTable.js
+++ src/genericComponents/variantTable/VariantTable.js
@@ -41,7 +41,31 @@ const ActiveLogDetails = () => (
       <div className="record_changed-item record__item">
         <div className="divider flex items-center">Hemi</div>
       </div>
-      <div className="record_time record__item">time</div>
+      <div className="record_time record__item">13:37, 4 June 2019</div>
+    </div>
+    <div className="record flex justify-between items-center">
+      <div className="record__user record__item">
+        <div className="divider flex items-center">
+          <AvatarName />
+          <span className="user-name">PA</span>
+        </div>
+      </div>
+      <div className="record_changed-item record__item">
+        <div className="divider flex items-center">Hemi</div>
+      </div>
+      <div className="record_time record__item">13:37, 4 June 2019</div>
+    </div>
+    <div className="record flex justify-between items-center">
+      <div className="record__user record__item">
+        <div className="divider flex items-center">
+          <AvatarName />
+          <span className="user-name">PA</span>
+        </div>
+      </div>
+      <div className="record_changed-item record__item">
+        <div className="divider flex items-center">Hemi</div>
+      </div>
+      <div className="record_time record__item">13:37, 4 June 2019</div>
     </div>
   </div>
 );
