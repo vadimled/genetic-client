0e679e7ba72a55981bc56d96271e7220214869ce
Vadim <vadim@grenesort.com>
2019-07-02 14:57:47 +0300

    tests for the actual value is shown in the document

--- src/pages/mainPage/components/tumorToolbar/TumorToolbar.test.js
+++ src/pages/mainPage/components/tumorToolbar/TumorToolbar.test.js
@@ -25,9 +25,12 @@ describe("Tumor Toolbar", () => {
 
     const locationSelect = getByTestId("tumor-type-select");
     expect(locationSelect).toBeInTheDocument();
-    store.dispatch(setTumorInfo({ type: "0" }));
+    store.dispatch(setTumorInfo({ type: "Test: type static text" }));
     expect(locationSelect).not.toBeInTheDocument();
-    expect(getByTestId("tumor-type-static-text")).toBeInTheDocument();
+  
+    const tumorTypeStaticText = getByTestId("tumor-type-static-text");
+    expect(tumorTypeStaticText).toBeInTheDocument();
+    expect(tumorTypeStaticText.innerHTML).toEqual("Test: type static text");
   });
 
   it("if TumorToolbar opened, check 'Location' select ", () => {
@@ -35,9 +38,12 @@ describe("Tumor Toolbar", () => {
 
     const locationSelect = getByTestId("tumor-location-select");
     expect(locationSelect).toBeInTheDocument();
-    store.dispatch(setTumorInfo({ location: "0" }));
+    store.dispatch(setTumorInfo({ location: "Test: location static text" }));
     expect(locationSelect).not.toBeInTheDocument();
-    expect(getByTestId("tumor-location-static-text")).toBeInTheDocument();
+    
+    const tumorLocationStaticText = getByTestId("tumor-location-static-text");
+    expect(tumorLocationStaticText).toBeInTheDocument();
+    expect(tumorLocationStaticText.innerHTML).toEqual("Test: location static text");
   });
 
   it("if TumorToolbar opened, check 'Percent' select ", () => {
@@ -45,9 +51,12 @@ describe("Tumor Toolbar", () => {
 
     const inputNumber = getByTestId("tumor-percent-inputNumber");
     expect(inputNumber).toBeInTheDocument();
-    store.dispatch(setTumorInfo({ percent: "1" }));
+    store.dispatch(setTumorInfo({ percent: 25 }));
     expect(inputNumber).not.toBeInTheDocument();
-    expect(getByTestId("tumor-percent-static-text")).toBeInTheDocument();
+    
+    const inputNumberText = getByTestId("tumor-percent-static-text");
+    expect(inputNumberText).toBeInTheDocument();
+    expect(inputNumberText.innerHTML).toEqual("25%");
   });
 
   it("if TumorToolbar opened, check 'tumor-panel-close-icon clicked", () => {
