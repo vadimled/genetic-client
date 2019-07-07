5c3512d94f2c2640676df7593965112c388e4770
Vadim <vadim@grenesort.com>
2019-07-07 15:23:41 +0300

    Variant Page: layout structured

--- src/pages/variantPage/VariantPage.js
+++ src/pages/variantPage/VariantPage.js
@@ -1,9 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import style from "./VariantPage.module.scss";
 // import PropTypes from 'prop-types';
 
 class VariantPage extends Component {
   render() {
-    return <h1>VariantPage</h1>;
+    return (
+      <article className={style["variant-page-wrapper"]}>
+        <section className="links-wrapper">
+          <h2>Links</h2>
+          <ul>
+            <li>
+              <a href="#">UCSD</a>
+            </li>
+            <li>
+              <a href="#">gnomId</a>
+            </li>
+            <li>
+              <a href="#">OMIM</a>
+            </li>
+          </ul>
+        </section>
+        <section className="main-wrapper">
+          <header className="main-header-data">GENEXYZ</header>
+          <section className="main-data">
+            <div className="history">History</div>
+            <div className="evidence">Evidence</div>
+          </section>
+        </section>
+      </article>
+    );
   }
 }
 
