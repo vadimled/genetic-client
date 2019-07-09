efc5770ade1c454340fccae6d2f113ec799b6b33
roman <romans@genesort.com>
2019-07-09 15:33:02 +0300

    WIP

--- src/genericComponents/variantTable/VariantTable.test.js
+++ src/genericComponents/variantTable/VariantTable.test.js
@@ -1,35 +1,15 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import "jest-dom/extend-expect";
 import VariantTable from './VariantTable';
 import { renderWithRedux } from "../../utils/test_helpers";
-import ActivityLog from "./components/ActivityLog";
 import { updateActivityLog } from "../../store/actions/tableActions";
+import { fireEvent } from "@testing-library/react";
+import "jest-dom/extend-expect";
 
-describe('VariantTable', () => {
-  it('VariantTable snapshot', () => {
-    const { asFragment } = render(
-      <VariantTable
-        onSelectRowKey={() => true}
-        handleZygosity={() => true}
-        handleVariantClass={() => true}
-        handelChrPosition={() => true}
-        handleSelectedRow={() => true}
-        handleSelectAllRows={() => true}
-        handleConfirmationStatus={() => true}
-        updateActivityLog={() => true}
-        setNotes={() => true}
-      />
-    );
-    expect(asFragment()).toMatchSnapshot();
-  });
-});
+describe("Variant Table Test", () => {
+  let getByTestId, zygosity;
 
-describe("Activity Log", () => {
-  let getByTestId, activityLogIcon, store, activityLogWrapper;
-
-  const activityLog = [
-    undefined,
+  const data = [
     {
       id: 0,
       key: 0,
@@ -51,81 +31,216 @@ describe("Activity Log", () => {
       coverage: 151,
       notes: "Sed recusandae in sint.",
       status: "pending"
-    },
-    0];
+    }];
+
+  // const activityLog = [
+  //   undefined,
+  //   {
+  //     id: 0,
+  //     key: 0,
+  //     gene: "ABCD1P4",
+  //     chrPosition: "Chr2:176882",
+  //     transcript: "NM_119875.6",
+  //     exon: 5,
+  //     alleleChange: "C > T",
+  //     alleleChangeLong: "fsghfsghsfghsfsfhsfh",
+  //     coding: "cAc/gTa",
+  //     codingLong: "aGc/tCg/zzzczzfzf",
+  //     protein: "nuo7b",
+  //     vaf: 76,
+  //     roi: true,
+  //     clinvar: "",
+  //     gnomAD: 3,
+  //     zygosity: "",
+  //     variantClass: "",
+  //     coverage: 151,
+  //     notes: "Sed recusandae in sint.",
+  //     status: "pending"
+  //   },
+  //   0];
 
   beforeEach(() => {
-    const queries = renderWithRedux(<ActivityLog {...activityLog} id={0} />);
+    const queries = renderWithRedux(<VariantTable
+      data={data}
+      handleSelectedRow={()=>{}}
+      handleSelectAllRows={()=>{}}
+      handleZygosity={()=>{}}
+      handleVariantClass={()=>{}}
+      handelChrPosition={()=>{}}
+      handleConfirmationStatus={()=>{}}
+      isAllRowSelected={false}
+      selectedRows={[]}
+      setNotes={()=>{}}
+      updateActivityLog={()=>{}}
+    />);
 
     getByTestId = queries.getByTestId;
 
-    store = queries.store;
-
-    activityLogIcon = getByTestId("activity-log-icon-0");
-
-    activityLogWrapper = getByTestId("activity-log-wrapper-0");
+    // zygosity = getByTestId("zygosity-select-0");
   });
 
-  it("activityLogIcon hover", () => {
+  it("activityLogNotes change", ()=> {
+
+    // const notes = getByTestId("notes");
+    const notes = document.getElementsByClassName("notes-content")[0];
+
+    expect(notes).toBeInTheDocument();
+
+    // const currNote = notes.innerHTML;
+    // const prevNote = notes.innerHTML = `${notes.innerHTML} test`;
+
+    console.log(notes.innerHTML);
+    //
+    const activityLogIcon  = getByTestId("activity-log-icon-0");
 
     expect(activityLogIcon).toBeInTheDocument();
-
-    expect(activityLogWrapper).toBeInTheDocument();
-
-    expect(activityLogWrapper).toHaveClass('activity-icon-wrapper');
-
-    store.dispatch(updateActivityLog({
-      prevValue: "LATH",
-      item: activityLog[1],
-      changedField: "variantClass"
-    }));
-
-    expect(activityLogWrapper).not.toHaveClass('disabled');
-
-    fireEvent.mouseOver(activityLogIcon);
-
-    const activityLogDetails = getByTestId("activity-log-details");
-
-    expect(activityLogDetails).toBeInTheDocument();
-
-    fireEvent.mouseLeave(activityLogWrapper);
-
-    expect(activityLogDetails).not.toBeInTheDocument();
-
-  });
-
-  it("activityLogIcon click", ()=> {
-
-    expect(activityLogIcon).toBeInTheDocument();
-
-    expect(activityLogWrapper).toBeInTheDocument();
-
-    expect(activityLogWrapper).toHaveClass('activity-icon-wrapper');
-
-    store.dispatch(updateActivityLog({
-      prevValue: "LATH",
-      item: activityLog[1],
-      changedField: "variantClass"
-    }));
-
-    expect(activityLogWrapper).not.toHaveClass('disabled');
 
     fireEvent.click(activityLogIcon);
 
     const activityLogPopup = getByTestId("activity-log-popup");
 
+
+    store.dispatch(updateActivityLog({
+      prevValue: "LATH",
+      item: data,
+      changedField: "variantClass"
+    }));
+
+
+
+
     expect(activityLogPopup).toBeInTheDocument();
-
-    const popupCloseBtn = document.querySelector(".ant-modal-close");
-
-    fireEvent.click(popupCloseBtn);
-
-    expect(activityLogPopup).not.toBeInTheDocument();
-
+    //
+    // expect(getByText(currNote)).toBeInTheDocument();
+    // expect(getByText(prevNote)).toBeInTheDocument();
 
   });
 
+  xit("zygosity click", () => {
+
+    expect(zygosity).toBeInTheDocument();
+
+
+    // fireEvent.change(zygosity, { target: { value: "homo" } });
+    fireEvent.click(zygosity);
+
+    // const option = document.getElementsByClassName("ant-select-dropdown-menu-item")[0];
+    // const option = getByText("Homo");
+
+    // console.log(option)
+
+    // fireEvent.click(option);
+    //
+    // const variantClass = getByTestId("variant-сlass-select-0");
+    // //
+    // expect(variantClass).toBeInTheDocument()
 
 
 
+  });
 });
+
+// describe("Activity Log", () => {
+//   let getByTestId, activityLogIcon, store, activityLogWrapper;
+//
+//   const activityLog = [
+//     undefined,
+//     {
+//       id: 0,
+//       key: 0,
+//       gene: "ABCD1P4",
+//       chrPosition: "Chr2:176882",
+//       transcript: "NM_119875.6",
+//       exon: 5,
+//       alleleChange: "C > T",
+//       alleleChangeLong: "fsghfsghsfghsfsfhsfh",
+//       coding: "cAc/gTa",
+//       codingLong: "aGc/tCg/zzzczzfzf",
+//       protein: "nuo7b",
+//       vaf: 76,
+//       roi: true,
+//       clinvar: "",
+//       gnomAD: 3,
+//       zygosity: "",
+//       variantClass: "",
+//       coverage: 151,
+//       notes: "Sed recusandae in sint.",
+//       status: "pending"
+//     },
+//     0];
+//
+//   beforeEach(() => {
+//     const queries = renderWithRedux(<ActivityLog {...activityLog} id={0} />);
+//
+//     getByTestId = queries.getByTestId;
+//
+//     store = queries.store;
+//
+//     activityLogIcon = getByTestId("activity-log-icon-0");
+//
+//     activityLogWrapper = getByTestId("activity-log-wrapper-0");
+//   });
+//
+//   it("activityLogIcon hover", () => {
+//
+//     expect(activityLogIcon).toBeInTheDocument();
+//
+//     expect(activityLogWrapper).toBeInTheDocument();
+//
+//     expect(activityLogWrapper).toHaveClass('activity-icon-wrapper');
+//
+//     store.dispatch(updateActivityLog({
+//       prevValue: "LATH",
+//       item: activityLog[1],
+//       changedField: "variantClass"
+//     }));
+//
+//     expect(activityLogWrapper).not.toHaveClass('disabled');
+//
+//     fireEvent.mouseOver(activityLogIcon);
+//
+//     const activityLogDetails = getByTestId("activity-log-details");
+//
+//     expect(activityLogDetails).toBeInTheDocument();
+//
+//     fireEvent.mouseLeave(activityLogWrapper);
+//
+//     expect(activityLogDetails).not.toBeInTheDocument();
+//
+//   });
+//
+//   it("activityLogIcon click", ()=> {
+//
+//     expect(activityLogIcon).toBeInTheDocument();
+//
+//     expect(activityLogWrapper).toBeInTheDocument();
+//
+//     expect(activityLogWrapper).toHaveClass('activity-icon-wrapper');
+//
+//     store.dispatch(updateActivityLog({
+//       prevValue: "LATH",
+//       item: activityLog[1],
+//       changedField: "variantClass"
+//     }));
+//
+//     expect(activityLogWrapper).not.toHaveClass('disabled');
+//
+//     fireEvent.click(activityLogIcon);
+//
+//     const activityLogPopup = getByTestId("activity-log-popup");
+//
+//     expect(activityLogPopup).toBeInTheDocument();
+//
+//     const popupCloseBtn = document.querySelector(".ant-modal-close");
+//
+//     fireEvent.click(popupCloseBtn);
+//
+//     expect(activityLogPopup).not.toBeInTheDocument();
+//
+//
+//   });
+//
+//
+//
+//
+// });
