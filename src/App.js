726505282bc8d6df56ed384c2f2f4a2501acfacb
Vadim <vadim@grenesort.com>
2019-07-23 16:37:22 +0300

    App.js refactoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from "react";
 import "Css/App.scss";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import MainPage from "Pages/mainPage";
 import Header from "Pages/mainPage/components/header";
 import ResultConfig from "Pages/mainPage/components/resultConfig";
 import VariantPage from "variantPage/";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Header />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    const { fetchUserData } = props;
+    fetchUserData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Header />
+        </div>
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/variant" exact component={VariantPage} />
+        </Switch>
+        <ResultConfig />
       </div>
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Route path="/variant" exact component={VariantPage} />
-      </Switch>
-      <ResultConfig />
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchUserData: data => dispatch(fetchUserData(data))
+  };
+}
+
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(App)
+);
