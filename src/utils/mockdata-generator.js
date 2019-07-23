b856491966c2794d6ed6c5aad9e2bd5c59cb9b3a
roman <romans@genesort.com>
2019-07-23 17:23:40 +0300

    WIP

--- src/utils/mockdata-generator.js
+++ src/utils/mockdata-generator.js
@@ -550,10 +550,10 @@ export const generateDNAVariantTableMockData = amount => {
       ]),
       zygosity: "",
 
-      // variantClassGermline: "unclassified",
-      variantClassGermline: faker.random.arrayElement(["unclassified", "path", "lpath", "vus", "lben", "ben"]),
-      // variantClassSomatic: "unclassified",
-      variantClassSomatic: faker.random.arrayElement(["unclassified", "tier1", "tier2", "tier3", "tier4"]),
+      variantClassGermline: "unclassified",
+      // variantClassGermline: faker.random.arrayElement(["unclassified", "path", "lpath", "vus", "lben", "ben"]),
+      variantClassSomatic: "unclassified",
+      // variantClassSomatic: faker.random.arrayElement(["unclassified", "tier1", "tier2", "tier3", "tier4"]),
       coverage: faker.random.number({ min: 100, max: 500 }),
       notes: faker.lorem.sentence(),
       status: process?.env?.NODE_ENV === 'test'
