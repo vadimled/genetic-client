ec5f2a2f5ec3156958515f5ce8f04c4b1df2bc58
roman <romans@genesort.com>
2019-07-24 14:48:54 +0300

    WIP

--- src/genericComponents/variantTable/components/TableSorter.js
+++ src/genericComponents/variantTable/components/TableSorter.js
@@ -1,60 +1,54 @@
-import React, {Component} from "react";
-// import { ReactComponent as DropdownArrow } from "Assets/dropdownArrow.svg";
+import React, { Component } from "react";
+import { ReactComponent as DropdownArrow } from "Assets/dropdownArrow.svg";
 import { SORTING_ORDER } from "../../../utils/constants";
+import cn from "classnames"
 
-
-class TableSorter extends Component{
-
+class TableSorter extends Component {
   state = {
     clicksCounter: 1
-  }
+  };
 
-  handleSort = () =>{
+  handleSort = () => {
+    const { setSort, field } = this.props;
 
-    const {setSort, field} = this.props;
+    const { clicksCounter } = this.state;
 
-    const {clicksCounter} = this.state;
-
-    if(this.state.clicksCounter < 2){
-      this.setState({clicksCounter: clicksCounter + 1});
-    }else if(clicksCounter >= 2){
-      this.setState({clicksCounter: 0});
+    if (this.state.clicksCounter < 2) {
+      this.setState({ clicksCounter: clicksCounter + 1 });
+    } else if (clicksCounter >= 2) {
+      this.setState({ clicksCounter: 0 });
     }
 
-    // let order = clicksCounter === 0 ? SORTING_ORDER["default"] ? clicksCounter === 1 ? SORTING_ORDER["ascending"] : SORTING_ORDER["descending"]
     let order = "";
 
-    switch(clicksCounter){
-      case 0: order = SORTING_ORDER["default"];
+    switch (clicksCounter) {
+      case 0:
+        order = SORTING_ORDER["default"];
         break;
-      case 1: order = SORTING_ORDER["ascending"];
+      case 1:
+        order = SORTING_ORDER["ascending"];
         break;
-      case 2: order = SORTING_ORDER["descending"];
+      case 2:
+        order = SORTING_ORDER["descending"];
         break;
     }
 
-    setSort({field, order});
-
-  }
-
-
+    setSort({ field, order });
+  };
 
   render() {
-    const {title} = this.props;
+    const { title } = this.props;
 
-    return(
-      <div className="flex items-center table-sorter-wrapper" onClick={() => this.handleSort()}>
-
-        <div className="table-sorter-title">
-          {title}
+    return (
+      <div
+        className="flex items-center table-sorter-wrapper"
+        onClick={() => this.handleSort()}
+      >
+        <div className="table-sorter-title">{title}</div>
+        <div className="flex flex-column">
+          <DropdownArrow className={cn("top-arrow", {"arrow-active top-arrow": this.state.clicksCounter === 0 })} />
+          <DropdownArrow className={cn({"arrow-active": this.state.clicksCounter === 2})} />
         </div>
-        {
-          /* <div className="flex flex-column">
-         <DropdownArrow className="top-arrow" onClick={()=> setSort({field: "vaf", order: SORTING_ORDER.descending})} />
-         <DropdownArrow onClick={()=> setSort({field: "vaf", order: SORTING_ORDER.ascending})} />
-       </div>*/
-        }
-
       </div>
     );
   }
