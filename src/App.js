70f718eaca97356843387606f844a9b9d2d8c803
Vadim <vadim@grenesort.com>
2019-07-24 14:12:23 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
 import React from "react";
 import "Css/App.scss";
-import { Route, Switch, withRouter } from "react-router-dom";
-import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 import MainPage from "Pages/mainPage";
 import Header from "Pages/mainPage/components/header";
 import ResultConfig from "Pages/mainPage/components/resultConfig";
 import VariantPage from "variantPage/";
-import { fetchCaseData } from "Actions/testActions";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    const { fetchCaseData } = props;
-    fetchCaseData("GS00115NP050818_TS1_01");
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Header />
-        </div>
-        <Switch>
-          <Route path="/" exact component={MainPage} />
-          <Route path="/variant" exact component={VariantPage} />
-        </Switch>
-        <ResultConfig />
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Header />
       </div>
-    );
-  }
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/variant" exact component={VariantPage} />
+      </Switch>
+      <ResultConfig />
+    </div>
+  );
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchCaseData: id => dispatch(fetchCaseData(id))
-  };
-}
-
-export default withRouter(
-  connect(
-    null,
-    mapDispatchToProps
-  )(App)
-);
+export default App;
