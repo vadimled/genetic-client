a1c0f66dcf7f52eb153b34266056576817062132
Vadim <vadim@grenesort.com>
2019-07-28 12:54:44 +0300

    commit

--- src/genericComponents/labeledTag/LabeledTag.js
+++ src/genericComponents/labeledTag/LabeledTag.js
@@ -5,31 +5,64 @@ import Tag from "GenericComponents/tag";
 import cn from "classnames";
 import { VARIANT_CLASS } from "Utils/constants";
 
-function LabeledTag({ label, typeData }) {
-  const type = typeData?.find(item => item.label === label);
-  return (
-    <div className={style["labeled-tag-wrapper"]}>
-      {type ? (
+function LabeledTag({ label, typeData, tagColor }) {
+  let type;
+
+  const renderLabeledTag = () => {
+    if (typeData) {
+      type = typeData?.find(item => item.label === label);
+
+      if (type) {
+        return (
+          <Fragment>
+            <Tag color={type.tagColor} />
+            <div
+              className={cn("label-text", {
+                unclassified: type.label === VARIANT_CLASS.unclassified.label
+              })}
+            >
+              {type.label}
+            </div>
+          </Fragment>
+        );
+      } else {
+        return <div className="label-text">{label}</div>;
+      }
+    } else if (tagColor) {
+      return (
         <Fragment>
-          <Tag color={type.tagColor} />
+          <Tag color={tagColor} />
           <div
             className={cn("label-text", {
-              unclassified: type.label === VARIANT_CLASS.unclassified.label
+              unclassified: label === VARIANT_CLASS.unclassified.label
             })}
           >
-            {type.label}
+            {label}
           </div>
         </Fragment>
-      ) : (
-        <div className="label-text">{label}</div>
-      )}
-    </div>
+      );
+    } else {
+      return (
+        <div
+          className={cn("label-text", {
+            unclassified: label === VARIANT_CLASS.unclassified.label
+          })}
+        >
+          {label}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className={style["labeled-tag-wrapper"]}>{renderLabeledTag()}</div>
   );
 }
 
 LabeledTag.propTypes = {
   label: PropTypes.string.isRequired,
-  typeData: PropTypes.array.isRequired
+  tagColor: PropTypes.string,
+  typeData: PropTypes.array
 };
 
 export default memo(LabeledTag);
