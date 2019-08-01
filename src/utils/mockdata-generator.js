34824f2419f098994059ae3cec3c31d3a219eceb
roman <romans@genesort.com>
2019-08-01 18:41:34 +0300

    WIP

--- src/utils/mockdata-generator.js
+++ src/utils/mockdata-generator.js
@@ -549,7 +549,9 @@ export const generateDNAVariantTableMockData = amount => {
         faker.random.number({ min: 5, max: 100 })
       ]),
       // zygosity: "notDefined",
-      zygosity: faker.random.arrayElement(["notDefined", "notReal", "insignificant", "somatic", "homo", "hemi", "hetro"]),
+      zygosity: faker.random.arrayElement([
+        "notDefined",
+        "notReal", "insignificant", "somatic", "homo", "hemi", "hetro"]),
 
       variantClassGermline: faker.random.arrayElement(["unclassified", "path", "lpath", "vus", "lben", "ben"  ]),
       variantClassSomatic: faker.random.arrayElement(["unclassified", "tier1", "tier2", "tier3", "tier4"]),
