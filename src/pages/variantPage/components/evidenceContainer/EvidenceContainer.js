f5c4a44c8b38dc6b97c03db6a31e15f81182d1d8
Vadim <vadim@grenesort.com>
2019-08-05 17:09:17 +0300

    SimpleButton component added

--- src/pages/variantPage/components/evidenceContainer/EvidenceContainer.js
+++ src/pages/variantPage/components/evidenceContainer/EvidenceContainer.js
@@ -1,40 +1,57 @@
 import React, { Component } from "react";
 import style from "./EvidenceContainer.module.scss";
 // import PropTypes from 'prop-types';
-import { Button, Tabs } from "antd";
+import { Tabs } from "antd";
 import TabPaneHeader from "variantComponents/evidenceContainer/components/tabPaneHeader";
 import PropTypes from "prop-types";
 import { getTabPaneHeaders } from "Store/selectors";
 import { connect } from "react-redux";
 import EvidenceTable from "variantComponents/evidenceContainer/components/evidenceTable";
+import SimpleButton from "GenericComponents/simpleButton";
 
 const { TabPane } = Tabs;
-const operations = <Button>+ Add Evidence</Button>;
 
 class EvidenceContainer extends Component {
   constructor(props) {
     super(props);
   }
-
+  
+  handleAddEvidence = () => {
+    console.log("handleAddEvidence");
+  };
+  
   render() {
     const { tabPaneHeaders /* , tabPaneBodies */ } = this.props;
     return (
       <div className={style["evidence-wrapper"]}>
         <div className="evidence-title">Evidence:</div>
-        <Tabs tabBarExtraContent={operations} size={"large"}>
-          {!!tabPaneHeaders && tabPaneHeaders.map((header, index) => {
-            // console.log(tabPaneBodies[index]);
-            return (
-              <TabPane
-                tab={
-                  <TabPaneHeader amount={header.length} title={header.title} />
-                }
-                key={index + 1}
-              >
-                {<EvidenceTable category={header.title}/>}
-              </TabPane>
-            );
-          })}
+        <Tabs
+          tabBarExtraContent={
+            <SimpleButton
+              className={"add-evidence-button-text"}
+              onClick={this.handleAddEvidence}
+              text={"+ Add Evidence"}
+            />
+          }
+          size={"large"}
+        >
+          {!!tabPaneHeaders &&
+            tabPaneHeaders.map((header, index) => {
+              // console.log(tabPaneBodies[index]);
+              return (
+                <TabPane
+                  tab={
+                    <TabPaneHeader
+                      amount={header.length}
+                      title={header.title}
+                    />
+                  }
+                  key={index + 1}
+                >
+                  {<EvidenceTable category={header.title} />}
+                </TabPane>
+              );
+            })}
         </Tabs>
       </div>
     );
