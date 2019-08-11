e780f1e4aca870633796b752cb72b1f34fed1c18
Vadim <vadim@grenesort.com>
2019-08-11 18:33:51 +0300

    date formatted

--- src/pages/variantPage/components/evidenceContainer/components/tableDateAndUser/TableDateAndUser.js
+++ src/pages/variantPage/components/evidenceContainer/components/tableDateAndUser/TableDateAndUser.js
@@ -2,11 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import style from "./TableDateAndUser.module.scss";
 
-function TableDateAndUser({date, user}) {
+function TableDateAndUser({ date, user }) {
+  const
+    options = { day: "2-digit", month: "short", year: "numeric" },
+    fDate = new Date(date),
+    formattedDate = fDate
+      .toLocaleDateString("en-GB", options)
+      .split(" ")
+      .join("/");
+
   return (
     <div className={style["table-date-and-user-wrapper"]}>
       <div className="table-date-and-user">
-        <div className="date">{date}</div>
+        <div className="date">{formattedDate}</div>
         <div className="user">{user.name}</div>
       </div>
     </div>
