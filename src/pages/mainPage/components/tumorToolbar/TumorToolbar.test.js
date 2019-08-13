eaab97ced71dfc55b0c322dcbb9020a6f83654f8
Vadim <vadim@grenesort.com>
2019-08-13 10:05:29 +0300

    test

--- src/pages/mainPage/components/tumorToolbar/TumorToolbar.test.js
+++ src/pages/mainPage/components/tumorToolbar/TumorToolbar.test.js
@@ -2,24 +2,10 @@ import React from "react";
 import { fireEvent } from "@testing-library/react";
 import "jest-dom/extend-expect";
 import { renderWithRedux } from "Utils/test_helpers";
-import App from "../../../../App";
-import { BrowserRouter as Router } from "react-router-dom";
 import TumorToolbar from "Pages/mainPage/components/tumorToolbar/TumorToolbar";
 import { setTumorInfo } from "Actions/testActions";
 
 describe("Tumor Toolbar", () => {
-  xit("if Icon Info reacted to click", () => {
-    const { getByTestId } = renderWithRedux(
-      <Router>
-        <App />
-      </Router>
-    );
-
-    const iconInfo = getByTestId("header-icon-info");
-    fireEvent.click(iconInfo);
-    expect(document.querySelector(".out")).toBeInTheDocument();
-  });
-
   it("if TumorToolbar opened, check 'Type' select ", () => {
     const { getByTestId, store } = renderWithRedux( <TumorToolbar  sidebarToggle />);
 
