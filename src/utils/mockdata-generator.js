88fe6a672f555899ca5447c0b3f25b5a8955e9ab
Vadim <vadim@grenesort.com>
2019-08-15 18:36:59 +0300

    real table: created Vaf mock

--- src/utils/mockdata-generator.js
+++ src/utils/mockdata-generator.js
@@ -525,8 +525,8 @@ export const generateDNAVariantTableMockData = amount =>
       ]),
       hgvs_c: faker.random.arrayElement(["gCc/gTc", "aGc/tCg", "cAc/gTa"]),
       hgvs_p: faker.random.alphaNumeric(5),
-      
-      vaf: faker.random.number(100) ,
+  
+      variant: faker.random.number({ min: 0.001, max: 1 }),
       coverage: faker.random.number({ min: 100, max: 500 }),
       
       hotSpot: faker.random.arrayElement([faker.random.boolean(), undefined]),
