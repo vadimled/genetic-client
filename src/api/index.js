1f59b65e11442d3d2d9413d8446e05bc93c61e18
roman <romans@genesort.com>
2019-08-25 16:06:19 +0300

    export table data

--- src/api/index.js
+++ src/api/index.js
@@ -118,5 +118,27 @@ export function fetchTableDataApi(action) {
 }
 
 export function exportTableApi(testId) {
-  return axios_based.post(`/tests/${testId}/export`, {gsId: testId});
+  return axios_based.post(`/tests/${testId}/export`, {gsId: testId}, {responseType: 'blob'}).then((response) => {
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${testId}.tsv`);
+    document.body.appendChild(link);
+    link.click();
+  });
+  // return axios({
+  //   url: `http://localhost:20211/tests/${testId}/export`,
+  //   method: 'POST',
+  //   responseType: 'blob', // important
+  //   body: {gsId: testId}
+  // })
+  //   .then((response) => {
+  //   const url = window.URL.createObjectURL(new Blob([response.data]));
+  //   const link = document.createElement('a');
+  //   link.href = url;
+  //   link.setAttribute('download', 'file.pdf');
+  //   document.body.appendChild(link);
+  //   link.click();
+  // });
 }
+
