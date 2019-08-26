7e12481860073cf28a4416ce0aff259f52997977
roman <romans@genesort.com>
2019-08-26 11:33:48 +0300

    mock has been created

--- src/api/index.js
+++ src/api/index.js
@@ -126,19 +126,5 @@ export function exportTableApi(testId) {
     document.body.appendChild(link);
     link.click();
   });
-  // return axios({
-  //   url: `http://localhost:20211/tests/${testId}/export`,
-  //   method: 'POST',
-  //   responseType: 'blob', // important
-  //   body: {gsId: testId}
-  // })
-  //   .then((response) => {
-  //   const url = window.URL.createObjectURL(new Blob([response.data]));
-  //   const link = document.createElement('a');
-  //   link.href = url;
-  //   link.setAttribute('download', 'file.pdf');
-  //   document.body.appendChild(link);
-  //   link.click();
-  // });
 }
 
