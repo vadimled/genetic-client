82bef67aff95be8dd38cffc31343517107e74cb1
@vadim17 <vadim@genesort.com>
2019-09-02 15:08:54 +0300

    link/not link mode separated

--- src/pages/variantPage/components/evidenceContainer/components/tableSourceDescription/TableSourceDescription.js
+++ src/pages/variantPage/components/evidenceContainer/components/tableSourceDescription/TableSourceDescription.js
@@ -1,12 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cn from "classnames";
 import style from "./TableSourceDescription.module.scss";
 
 function TableSourceDescription({ source, description }) {
+  const isSourceLink = () => {
+    const expression = /https?:\/\/[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi;
+    return source.match(new RegExp(expression));
+  };
+
   return (
     <div className={style["table-source-description-wrapper"]}>
       <div className="table-source-description">
-        <div className="source">{source}</div>
+        <div className={cn("source", { link: isSourceLink() })}>
+          {isSourceLink() ? (
+            <a href={`${source}`} target="_blank" rel="noopener noreferrer">
+              {source}
+            </a>
+          ) : (
+            source
+          )}
+        </div>
         <div className="description">{description}</div>
       </div>
     </div>
