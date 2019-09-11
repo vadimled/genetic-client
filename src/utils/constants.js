1b24a9727936409909cb9f06a9960752de9e49d9
roman <romans@genesort.com>
2019-09-11 14:16:37 +0300

    wip

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -179,7 +179,8 @@ export const CONFIRMATION_VALUES = {
     label: "Not Confirmed",
     iconImg: ImgCircleX
   },
-  PENDING: { value: "PENDING", label: "Pending", iconImg: ImgCircleClock }
+  PENDING: { value: "PENDING", label: "Pending", iconImg: ImgCircleClock },
+  // UNCHECK: { value: "UNCHECK", label: "Uncheck", iconImg: "" }
 };
 
 export const ALERT_STATUSES = {
