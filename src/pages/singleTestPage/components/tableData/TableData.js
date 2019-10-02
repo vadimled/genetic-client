36c11f937df207e97d1c4af2bc87ba386e412b21
Shahar Porat <shahar@genesort.com>
2019-10-02 08:17:59 +0000

    Apply suggestion to src/pages/singleTestPage/components/tableData/TableData.js

--- src/pages/singleTestPage/components/tableData/TableData.js
+++ src/pages/singleTestPage/components/tableData/TableData.js
@@ -70,7 +70,7 @@ class TableData extends Component {
             isAllRowSelected={isAllRowSelected}
             selectedRows={selectedRows}
             setNotes={setNotes}
-            setSort={this.handleSort}Б
+            setSort={this.handleSort}
             sortOrder={sortOrder}
             sortParam={sortParam}
             testId={testId}
