567c49d483c7d19cf090cadf89ae1fd854ce1dcd
roman <romans@genesort.com>
2019-10-03 12:34:04 +0300

    wip

--- src/pages/finalReportPage/FinalReportPage.js
+++ src/pages/finalReportPage/FinalReportPage.js
@@ -1,6 +1,81 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import style from "./FinalReportPage.module.scss";
+import { Table, Divider, Tag } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <span>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'loser') {
+            color = 'volcano';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a>Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a>Delete</a>
+      </span>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+];
 
 class FinalReportPage extends Component {
 
@@ -9,8 +84,10 @@ class FinalReportPage extends Component {
     return (
       <div className={`${style["final-report-page-wrapper"]} flex justify-between`}>
         <div className="main-content">
-          <div>Table 1</div>
-          <div>Table 2</div>
+          <div className="actionable-table">Table 1</div>
+          <div className="variant-table">
+            <Table columns={columns} dataSource={data} />
+          </div>
         </div>
         <div className="sidebar">
           Sidebar
