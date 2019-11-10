bc287bd098f53664f5b41c3891250fd332d85b0d
Vadim <vadim@grenesort.com>
2019-11-10 14:54:50 +0200

    MultiLineText component fixed

--- src/genericComponents/multiLineText/MultiLineText.js
+++ src/genericComponents/multiLineText/MultiLineText.js
@@ -5,14 +5,15 @@ import cn from "classnames";
 import { Tooltip } from "antd";
 
 const MultiLineText = ({ source, className }) => {
-  console.log(source);
   return (
     <div className={cn(style["multiLine-text-wrapper"], className)}>
       {Object.keys(source).map((key, index) => {
-        return source[key] &&(
-          <Tooltip key={index} placement="topLeft" title={source[key]}>
-            <div className="multiLine-text">{source[key]}</div>
-          </Tooltip>
+        return (
+          source[key] && (
+            <Tooltip key={index} placement="topLeft" title={source[key]}>
+              <div className="multiLine-text">{source[key]}</div>
+            </Tooltip>
+          )
         );
       })}
     </div>
