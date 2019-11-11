bf0af99bd5a2c8d275362246ea4dce180b7a1621
@vadim17 <vadim@genesort.com>
2019-11-11 17:17:07 +0200

    refactoring

--- src/pages/finalReportPage/components/finalReportActionableTable/components/actionableDetailsContainer/ActionableDetailsContainer.js
+++ src/pages/finalReportPage/components/finalReportActionableTable/components/actionableDetailsContainer/ActionableDetailsContainer.js
@@ -6,8 +6,7 @@ import { connect } from "react-redux";
 import SimpleButton from "GenericComponents/simpleButton";
 import {
   ACTIONABLE_CATEGORIES_OPTIONS,
-  ACTIONABLE_TABS_VALUES,
-  TEXTS
+  ACTIONABLE_TABS_VALUES
 } from "Utils/constants";
 import {
   getCurrentActionableTab,
@@ -19,7 +18,6 @@ import {
 import {
   setCurrentActionableTab,
   saveExpandedTextAreaContent
-  // setExpandedInterpretationTextArea
 } from "Actions/finalReportAction";
 import ActionableDetailsTabPaneHeader from "../actionableDetailsTabPaneHeader";
 import ExpandedInterpretation from "./components/expandedInterpretation";
@@ -27,51 +25,7 @@ import ExpandedInterpretation from "./components/expandedInterpretation";
 const { TabPane } = Tabs;
 
 class ActionableDetailsContainer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showPopupDelete: false
-    };
-  }
-
-  handleAddActionable = () => {
-    this.props.onAction({
-      actionSlideBarStatus: true,
-      mode: TEXTS.add
-    });
-  };
-
-  handleEditEntry = (e, id) => {
-    const { data } = this.props;
-    this.props.onAction({
-      actionSlideBarStatus: true,
-      id,
-      mode: TEXTS.edit,
-      data: data[id]
-    });
-  };
-
-  handleDeleteEntry = (e, id) => {
-    this.setState({ showPopupDelete: !this.state.showPopupDelete });
-    this.props.onAction({
-      actionSlideBarStatus: false,
-      id,
-      mode: TEXTS.delete
-    });
-  };
-
-  handleDeleteYes = () => {
-    const { deleteEntry, deleteData } = this.props;
-    deleteEntry(deleteData);
-    this.setState({ showPopupDelete: !this.state.showPopupDelete });
-  };
-
-  handleDeleteNo = () => {
-    this.setState({ showPopupDelete: !this.state.showPopupDelete });
-    this.props.cleanData();
-  };
-
+  
   onTabClicked = key => {
     this.props.setCurrentActionableTab(key);
   };
@@ -114,13 +68,6 @@ class ActionableDetailsContainer extends Component {
     return (
       <div className={style["actionable-details-container-wrapper"]}>
         <Tabs
-          // tabBarExtraContent={
-          //   <SimpleButton
-          //     className={"actionable-details-done"}
-          //     // onClick={this.handleAddActionable}
-          //     text={"done"}
-          //   />
-          // }
           size={"large"}
           activeKey={currentActionableTab}
           onChange={this.onTabClicked}
@@ -167,7 +114,6 @@ const mapStateToProps = state => {
 
 function mapDispatchToProps(dispatch) {
   return {
-    // onAction: status => dispatch(setActionableActionMode(status)),
     setExpandedInterpretationTextArea: data =>
       dispatch(saveExpandedTextAreaContent(data)),
     setCurrentActionableTab: key => dispatch(setCurrentActionableTab(key))
