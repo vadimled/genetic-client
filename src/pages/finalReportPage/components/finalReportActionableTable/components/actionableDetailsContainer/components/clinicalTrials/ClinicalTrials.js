a3c7d2955ef1f62b7bdd31f2daea89d81d72051f
Vadim <vadim@grenesort.com>
2019-11-19 11:29:43 +0200

    clinical trials component (for third Tab)

---
+++ src/pages/finalReportPage/components/finalReportActionableTable/components/actionableDetailsContainer/components/clinicalTrials/ClinicalTrials.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Input } from "antd";
+import PropTypes from "prop-types";
+import style from "./ClinicalTrials.module.scss";
+import cn from "classnames";
+import { ReactComponent as OkIcon } from "Assets/ok.svg";
+
+const { TextArea } = Input;
+const ClinicalTrials = ({ data, onChange }) => {
+  const renderDescription = (
+    { source_description, drug_name, source_description_saved },
+    index
+  ) => {
+    return (
+      <div className="source-description-wrapper" key={index}>
+        <label htmlFor={"source-description-textarea"}>{drug_name}</label>
+        <TextArea
+          id={`source-description-textarea-${index}`}
+          autosize
+          value={source_description}
+          onChange={onChange}
+          placeholder={"Type here"}
+        />
+        <div className="icon-saved">
+          <div className={cn("ok-icon", { active: source_description_saved })}>
+            <OkIcon />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={style["clinical-trials-wrapper"]}>
+      {data && data.map((obj, index) => renderDescription(obj, index))}
+    </div>
+  );
+};
+
+ClinicalTrials.propTypes = {
+  data: PropTypes.array,
+  onChange: PropTypes.func
+};
+
+export default ClinicalTrials;
